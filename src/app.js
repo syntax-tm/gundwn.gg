065f3fa30400c5099790cc242c16e03b210f2840
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2023-11-10 21:13:28 -0500

    Added expanded section for PC Setup, reorganized CSS, and more.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import p from './img/pikachu_sq.gif';
 import h3 from './img/halo_3_general.png';
-import h3b from './img/halo_3_general_bw.png';
-import './style.css';
+import pcpartpicker from './img/pcpartpicker.png';
+import amd from './img/amd.png';
+import amd2 from './img/amd2.png';
+import nvidia from './img/nvidia.png';
+import asusRog from './img/asus_rog.png';
+import corsair from './img/corsair.png';
+import corsair2 from './img/corsair2.png';
+import leopold from './img/leopold.png';
+import logi from './img/logi.png';
+import kvm from './img/kvm.png';
+import gskill from './img/gskill.png';
+import astro from './img/astro.png';
+import desk from './img/desk.png';
+//import zotac from './img/zotac.png';
+import samsung from './img/samsung.png';
+import samsung2 from './img/samsung_light.png';
+import './css/style.css';
 import 'particles.js';
 import $ from 'jquery';
 const particlesJson = require('./config/particles.json');
@@ -12,8 +27,24 @@ import links from './config/links.json';
 
 particlesJS('background', particlesJson);
 
+$('#pcpartpicker').attr('src', pcpartpicker);
+$('#cpu').attr('src', amd);
+$('#psu').attr('src', corsair);
+$('#display').attr('src', samsung);
+$('#mobo').attr('src', asusRog);
+$('#gpu').attr('src', nvidia);
+$('#keyboard').attr('src', leopold);
+$('#ram').attr('src', gskill);
+$('#headset').attr('src', astro);
+$('#mouse').attr('src', corsair);
+$('#speakers').attr('src', logi);
+$('#kvm').attr('src', kvm);
+$('#storage').attr('src', samsung);
+$('#chair').attr('src', corsair);
+$('#desk').attr('src', desk);
+
 $('.profile-image').attr('src', p);
-$('#footer-image').attr('src', h3b);
+$('#footer-image').attr('src', h3);
 
 //$('#footer-image').hover(
 //    function() {
@@ -24,7 +55,8 @@ $('#footer-image').attr('src', h3b);
 //    }
 //)
 
-$('a').attr('target', '_blank');
 
+
+$('a').attr('target', '_blank');
 
 console.log(links);
