72a0aa39c8bde8bcf396dd58c613e62ec6cc020b
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2023-11-11 23:55:12 -0500

    Changed setup submenu to load from the config and updated the main menu.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import p from './img/pikachu_sq.gif';
+import p from './img/pikachu.gif';
+import p_sq from './img/pikachu_sq.gif';
 import h3 from './img/halo_3_general.png';
 import pcpartpicker from './img/pcpartpicker.png';
 import amd from './img/amd.png';
@@ -19,9 +20,55 @@ import './css/style.css';
 import 'particles.js';
 import $ from 'jquery';
 const particlesJson = require('./config/particles.json');
-import links from './config/links.json';
+import menuConfig from './config/menu.json';
 
 particlesJS('background', particlesJson);
+
+$('meta[property="og:image"]').attr('content', p);
+$('meta[property="twitter:image"]').attr('content', p);
+
+$('#profile-image').attr('src', p_sq);
+$('#footer-image').attr('src', h3);
+
+function createSubMenuItem(options)
+{
+    if (options.type === "separator")
+    {
+        $('<hr/>').appendTo('#pc-submenu');
+        return;
+    }
+    if (options.type === "button")
+    {
+        var html = `<a href="${options.link}" role="btn" height="auto" class="text-decoration-none">
+            <button>
+                <div class="d-flex align-content-center align-items-center w-100 justify-content-center" style="text-align: center; align-self: center; align-items: center;">
+                <!-- <img id="pcpartpicker" class="object-fit-scale" style="width: 35px; height: 35px;"></img> -->
+                <i class="fa-solid fa-external-link m-2"></i>
+                <div style="">View On PC Part Picker</div>
+                </div>
+            </button>
+        </a>`;
+
+        $(html).appendTo('#pc-submenu');
+        return;
+    }
+
+    var html = `<button class="btn-submenu">
+                    <img id="${options.id}" class="btn-image list-item-btn-image object-fit-scale"></img>
+                    <div class="vr1"></div>
+                    <div class="category">${options.shortCategory}</div>
+                    <div class="vr"></div>
+                    <div class="flex-grow-1 pc-part-name" title="${options.description}">${options.description}</div>
+                </button>`;
+
+    $(html).appendTo('#pc-submenu');
+}
+
+var items = menuConfig.menu[0].items;
+
+items.forEach(function (item) {
+    createSubMenuItem(item);
+});
 
 $('#pcpartpicker').attr('src', pcpartpicker);
 $('#pcpartpicker').attr('title', 'PC Part Picker');
@@ -53,17 +100,5 @@ $('#chair').attr('src', corsair);
 $('#chair').attr('title', 'Corsair');
 $('#desk').attr('src', desk);
 $('#desk').attr('title', 'Desk');
-
-$('.profile-image').attr('src', p);
-$('#footer-image').attr('src', h3);
-
-$('.pc-part-name').each(function(index) {
-    const el = $(this);
-    const val = el.text();
-
-    el.attr('title', val);
-
-    console.log(val);
-});
 
 $('a').attr('target', '_blank');
