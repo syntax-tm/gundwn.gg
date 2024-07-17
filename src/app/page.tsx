d9a6448d3949c7413a43271f97b371ab7889886d
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2024-07-17 07:30:31 -0400

    Added modal dynamic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Background from '@/components/background/background';
 import Clock from "@/components/clock/Clock";
 import dynamic from "next/dynamic";
 import { useRef, useState, ReactNode, useEffect, Dispatch, SetStateAction, CSSProperties } from "react";
-import Modal from "@/components/modal/Modal";
+//import Modal from "@/components/modal/Modal";
 
 const Menu = dynamic(() => import('@/components/xmb-menu/xmb-menu'), { ssr: false });
-//const Modal = dynamic(() => import('@/components/modal/modal'), { ssr: false })
+const Modal = dynamic(() => import('@/components/modal/Modal'), { ssr: false })
 
 export default function Home() {
 
